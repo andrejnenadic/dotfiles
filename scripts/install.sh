647c21858430b684e8d941dedff3a390d5b24ff8
Andrej235 <andrej.nenadic06@gmail.com>
2026-08-04 21:13:13 +0200

    Add stow to install script, fix ocr

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-read -p "Do you want keyd configured (requires root)? (y/n) " -n 1 -r
+stow -vR -t ~ bin
+stow -vR -t ~ wallpapers
+stow -vR -t ~ hypr
+stow -vR -t ~ kitty
+stow -vR -t ~ rofi
+
+read -p "Do you want to configure keyd (requires root)? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo stow -t / keyd
+    sudo keyd reload
 fi
