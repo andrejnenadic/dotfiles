1478b0797286980688d4c1360ee15cf34d08fe3d
Andrej235 <andrej.nenadic06@gmail.com>
2026-08-10 22:45:27 +0200

    Change nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -21,6 +21,39 @@ vim.api.nvim_create_autocmd('FileType', {
 require("oil").setup()
 vim.keymap.set("n", "-", "<CMD>Oil<CR>")
 
+vim.diagnostic.config({
+    virtual_text = true
+})
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
+-- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
+-- See `:h 'clipboard'`
+vim.api.nvim_create_autocmd('UIEnter', {
+    callback = function()
+        vim.o.clipboard = 'unnamedplus'
+    end
+})
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.cursorline = true -- Highlight the line where the cursor is on.
+vim.o.scrolloff = 10 -- Keep this many screen lines above/below the cursor.
+vim.o.list = true -- Show <tab> and trailing spaces.
+
+-- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
+vim.o.confirm = true
+
+-- Highlight when yanking (copying) text.
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    callback = function()
+        vim.hl.on_yank()
+    end
+})
+
