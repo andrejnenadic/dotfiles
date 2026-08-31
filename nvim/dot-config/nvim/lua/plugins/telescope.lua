94b34e918c46844f3495b3e7c13e24ca5737ea5a
andrejnenadic <andrej.nenadic06@gmail.com>
2026-08-31 16:20:14 +0200

    Fix telescope config

--- nvim/dot-config/nvim/lua/plugins/telescope.lua
+++ nvim/dot-config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,3 @@
-local telescope = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", telescope.find_files, {
-    desc = "Telescope find files"
-})
-vim.keymap.set("n", "<leader>fg", telescope.live_grep, {
-    desc = "Telescope live grep"
-})
-
 return {{
     'nvim-telescope/telescope.nvim',
     version = '*',
